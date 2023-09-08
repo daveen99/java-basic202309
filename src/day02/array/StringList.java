517ae784cf6d30883a898d4738c4f65b1095d190
다빈 <prospecs1039@daum.net>
2023-09-08 09:27:50 +0900

    # 메서드 연습문제 +강사님

--- src/day02/array/StringList.java
+++ src/day02/array/StringList.java
@@ -76,6 +76,7 @@ public class StringList {
         sArr = temp;
     }
 
+    // 자료 유무 확인
     boolean includes(String target) {
         for (int i = 0; i < sArr.length; i++) {
             if(sArr[i].equals(target)) {
@@ -85,6 +86,7 @@ public class StringList {
         return false;
     }
 
+    // 인덱스 탐색
     int indexOf(String target) {
         for (int i = 0; i < sArr.length; i++) {
             if(sArr[i].equals(target)) {
@@ -110,4 +112,55 @@ public class StringList {
     void printArray() {
         System.out.println(Arrays.toString(sArr));
     }
+
+
+    // 강사님
+//    // 인덱스 탐색 (indexOf)
+//    public int indexOf(String target) {
+//        for (int i = 0; i < sArr.length; i++) {
+//            if (target.equals(sArr[i])) {
+//                return i;
+//            }
+//        }
+//        return -1;
+//    }
+//
+//    // 자료 유무 확인 (includes)
+//    public boolean includes(String target) {
+//        return indexOf(target) != -1;
+//    }
+//
+//    // 중간 삭제 (remove) : 인덱스로 삭제
+//    public String remove(int index) {
+//        if (index < 0 || index > sArr.length - 1) return null;
+//
+//        String targetData = sArr[index];
+//        for (int i = index; i < sArr.length - 1; i++) {
+//            sArr[i] = sArr[i + 1];
+//        }
+//        pop();
+//        return targetData;
+//    }
+//    // 중간 삭제 (remove) : 값으로 삭제
+//    public String remove(String target) {
+//        return remove(indexOf(target));
+//    }
+//
+//    // 중간 삽입 (insert)
+//    public void insert(int index, String newData) {
+//
+//        if (index < 0 || index > sArr.length - 1) return;
+//        if (index == sArr.length - 1) push(newData);
+//
+//        String[] temp = new String[sArr.length + 1];
+//        for (int i = 0; i < sArr.length; i++) {
+//            temp[i] = sArr[i];
+//        }
+//        for (int i = temp.length - 1; i > index; i—) {
+//            temp[i] = temp[i-1];
+//        }
+//        temp[index] = newData;
+//        sArr = temp;
+//    }
+
 }
