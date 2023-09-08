0edc15e5906d311a34e24f90751fd6216f62a174
다빈 <prospecs1039@daum.net>
2023-09-08 17:51:33 +0900

    # 회원관리 프로그램 완료

--- src/day03/member/MemberRepository.java
+++ src/day03/member/MemberRepository.java
@@ -1,5 +1,7 @@
 package day03.member;
 
+import java.util.Arrays;
+
 // 책임(역할): 회원을 여러명 저장관리하는 클래스
 public class MemberRepository {
 
@@ -15,11 +17,26 @@ public class MemberRepository {
                 new Member(2, "fff@ggg.com", "4567",
                         "팥죽이", Gender.FEMALE, 30),
                 new Member(3, "xxx@vvv.com", "8765",
+                        "카레맨", Gender.FEMALE, 45),
+                new Member(3, "xxx@vvv.com", "8765",
+                        "카레맨", Gender.FEMALE, 45),
+                new Member(3, "xxx@vvv.com", "8765",
+                        "카레맨", Gender.FEMALE, 45),
+                new Member(3, "xxx@vvv.com", "8765",
+                        "카레맨", Gender.FEMALE, 45),
+                new Member(3, "xxx@vvv.com", "8765",
+                        "카레맨", Gender.FEMALE, 45),
+                new Member(3, "xxx@vvv.com", "8765",
+                        "카레맨", Gender.FEMALE, 45),
+                new Member(3, "xxx@vvv.com", "8765",
+                        "카레맨", Gender.FEMALE, 45),
+                new Member(3, "xxx@vvv.com", "8765",
                         "카레맨", Gender.FEMALE, 45)
         };
+        this.removeMembers = new Member[0];
     }
 
-    /*
+    /**
         * 모든 회원정보를 출력하는 메서드
      */
     void showMembers() {
@@ -30,7 +47,7 @@ public class MemberRepository {
         }
     }
 
-    /*
+    /**
         * 새로운 회원정보를 추가하는 메서드
      */
     boolean addMember(Member newMember) {
@@ -45,19 +62,140 @@ public class MemberRepository {
         return true;
     }
 
-    /*
+    /**
         * 이메일의 중복여부를 확인하는 메서드
+        * @param1 email - 중복검사 대상 이메일
+        * @return - 중복되었다면 true, 아니라면 false
      */
+    boolean isDuplicatedEmail(String email) {
+        for (Member member : memberList) {
+            if(email.equals(member.email)){
+                return true;
+            }
+        }
+        return false;
+    }
 
-    /*
-        * 이메일을 통해 특정 회원 객체를 찾아서 반환하는 메서드
+    /**
+     * 이메일을 통해 특정 회원 객체를 찾아서 반환하는 메서드
+     * @param1 email - 탐색할 멤버객체의 이메일
+     * @return - 해당 이메일과 일치하는 회원의 모든정보 객체
+     * 이메일이 일치하지 않으면 null값 반환
      */
+    Member findMemberByEmail(String email) {
+        for (Member member : memberList) {
+            if (email.equals(member.email)) {
+                return member;
+            }
+        }
+        for (Member member2 : removeMembers) {
+            if (email.equals(member2.email)){
+                return member2;
+            }
+        }
+        return null;
+    }
 
     /*
         * 비밀번호를 수정하는 메서드
      */
+    void updatePassword(String newPassword, String email) {
+        Member member = findMemberByEmail(email);
+        member.password = newPassword;
+    }
+
+    /**
+     * 배열의 길이를 반환하는 메서드
+     */
+    public int howManyUsers() {
+        return memberList.length;
+    }
 
     /*
         * 회원탈퇴를 처리하는 메서드
      */
+    public void memberDelete(Member member) {
+
+        for (int i = 0; i < memberList.length; i++) {
+            if(member.equals(memberList[i])){
+                deletePush(member);
+                deleteM(i);
+            }
+        }
+
+
+    }
+
+    public void deleteM(int index) {
+        Member[] temp = new Member[memberList.length-1];
+        for (int i = index; i < memberList.length-1; i++) {
+            memberList[i] = memberList[i+1];
+        }
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = memberList[i];
+        }
+
+        memberList = temp;
+
+        System.out.println("# 회원 삭제 완료.");
+    }
+
+    public void deletePush(Member member) {
+        Member[] temp;
+
+        if(removeMembers.length < 1) {
+            temp = new Member[1];
+            temp[0] = member;
+        } else {
+            temp = new Member[removeMembers.length+1];
+            for (int i = 0; i < removeMembers.length; i++) {
+                temp[i] = removeMembers[i];
+            }
+            temp[removeMembers.length+1] = member;
+        }
+        removeMembers = temp;
+
+
+        System.out.println("# = 삭제대기열 =");
+        for (Member removeMember : removeMembers) {
+            System.out.println(removeMember.inform());
+        }
+    }
+
+
+    public void restoreMethod(Member member) {
+        Member[] temp = new Member[memberList.length+1];
+        for (int i = 0; i < memberList.length; i++) {
+            temp[i] = memberList[i];
+        }
+        temp[memberList.length] = member;
+        memberList = temp;
+
+        if (removeMembers.length == 1) {
+            removeMembers = new Member[0];
+        } else {
+            temp = new Member[removeMembers.length-1];
+            int index = 0;
+            for (int i = 0; i < removeMembers.length; i++) {
+                if (removeMembers[i] == member) {
+                    index = i;
+                }
+            }
+
+            for (int i = index; i < removeMembers.length-1; i++) {
+                    removeMembers[i] = removeMembers[i+1];
+            }
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = removeMembers[i];
+            }
+            removeMembers = temp;
+
+        }
+
+
+        System.out.println("# = 삭제대기열 =");
+        for (Member removeMember : removeMembers) {
+            System.out.println(removeMember.inform());
+        }
+    }
 }
--- src/day03/member/MemberView.java
+++ src/day03/member/MemberView.java
@@ -18,13 +18,20 @@ public class MemberView {
      * 초기 메인 화면을 출력
      */
     void mainView() {
+        int i = mr.howManyUsers();
+
         System.out.println("\n##### 회원 관리 시스템 #####");
-        System.out.println("* 1. 회원 정보 등록하기");
+        if (i == 10) {
+            System.out.println("* ※ 현재 회원수(10명) [인원초과] ※");
+        } else {
+            System.out.println("* 1. 회원 정보 등록하기");
+        }
         System.out.println("* 2. 개별회원 정보 조회하기");
         System.out.println("* 3. 전체회원 정보 조회하기");
         System.out.println("* 4. 회원 정보 수정하기");
         System.out.println("* 5. 회원 정보 삭제하기");
         System.out.println("* 6. 프로그램 끝내기");
+        System.out.println("* 7. 회원 복구하기");
         System.out.println("=============================");
     }
 
@@ -41,14 +48,18 @@ public class MemberView {
                     signUp();
                     break;
                 case "2":
+                    showMember();
+                    stop();
                     break;
                 case "3":
                     mr.showMembers();
                     stop();
                     break;
                 case "4":
+                    changePassword();
                     break;
                 case "5":
+                    deleteMember();
                     break;
                 case "6":
                     String answer = input("# 정말로 종료합니까? [y/n] : ");
@@ -59,29 +70,154 @@ public class MemberView {
                         continue;
                     }
                     break;
+                case "7":
+                    memberRestore();
+                    break;
                 default:
                     System.out.println("\n# 메뉴 번호를 다시 입력하세요");
             }
         }
     }
 
+    private void memberRestore() {
+        if (mr.removeMembers.length == 0) {
+            System.out.println("# 삭제된 회원정보가 없습니다.");
+            stop();
+            return;
+        }
+        Member member = checkEmailInput();
+        if(checkPasswordInput(member)){
+            if (mr.howManyUsers() == 10){
+                return;
+            }
+            System.out.println("# 정보 확인완료, 복구 시작.");
+
+            mr.restoreMethod(member);
+            System.out.println("# 복구 완료.");
+            stop();
+        };
+    }
+
+    // 회원을 삭제한다
+    private void deleteMember() {
+        // 이메일을 입력받고 확인
+        Member member = checkEmailInput();
+        if (member != null) {
+            if(checkPasswordInput(member)){
+                mr.memberDelete(member);
+                stop();
+            };
+        }
+    }
+
+    private void changePassword() {
+        // 이메일을 입력받음
+        String email = input("# 수정 대상의 이메일: ");
+
+        // 수정 대상 탐색
+        Member member = mr.findMemberByEmail(email);
+
+        // 회원이 탐색됨
+        if (member != null) {
+            System.out.printf("# %s님의 비밀번호를 변경합니다.\n", member.memberName);
+            String newPassword = input("# 새로운 비밀번호: ");
+
+            // 새로운 비밀번호가 기존 비밀번호와 같은 경우
+            if (member.password.equals(newPassword)) {
+                System.out.println("# 기존 비밀번호와 동일합니다.");
+                stop();
+                return;
+            }
+
+            mr.updatePassword(newPassword, email);
+            System.out.println("# 비밀번호 변경이 완료되었습니다.");
+        } else {
+            System.out.println("\n# 조회 결과가 없습니다.");
+        }
+        stop();
+    }
+
     // 회원가입 입출력을 처리하는 메서드
     void signUp() {
+        int i = mr.howManyUsers();
+        if (i == 10) {
+            System.out.println("# 회원수가 10명이 넘어 포화상태입니다. [가입불가]");
+            stop();
+            return;
+        }
 
-        String email = input("# 이메일: ");
+        String email = checkDuplicateEmailInput();
         String name = input("# 이름: ");
         String pw = input("# 패스워드: ");
-        String gender = input("# 성별(M/F): ");
+        Gender gender = checkCorrectGenderInput();
         String age = input("# 나이: ");
 
         // 실제 배열에 저장 명령
         // 랜덤 아이디 생성 ( 1 ~ 999999 )
-        int randomId = (int)(Math.random() * 999999) + 1;
-        Member newMember = new Member(randomId, email, pw, name, Gender.valueOf(gender), Integer.parseInt(age));
+        int randomId = (int) (Math.random() * 999999) + 1;
+        Member newMember = new Member(randomId, email, pw, name, gender, Integer.parseInt(age));
         mr.addMember(newMember);
 
         System.out.println("회원가입 완료");
         stop();
+    }
+
+    private Gender checkCorrectGenderInput() {
+        while (true) {
+            String inputGender = input("# 성별 [M/F]: ");
+            switch (inputGender.toUpperCase()) {
+                case "M":
+                    return Gender.MALE;
+                case "F":
+                    return Gender.FEMALE;
+                default:
+                    System.out.println("# 성별을 M또는 F로만 입력해주세요.");
+            }
+        }
+
+    }
+
+    private String checkDuplicateEmailInput() {
+        while (true) {
+            String email = input("# 이메일: ");
+            if (!mr.isDuplicatedEmail(email)) {
+                return email;
+            }
+            System.out.println("#이메일이 중복되었습니다.");
+        }
+    }
+
+    // 개별회원 이메일 검사
+    private Member checkEmailInput() {
+        String email = input("# 이메일 검색: ");
+        Member member = mr.findMemberByEmail(email);
+
+        if (member != null) {
+            return member;
+        } else {
+            System.out.println("# 존재하지 않는 이메일입니다.");
+            return null;
+        }
+    }
+    // 패스워드 검사
+    private boolean checkPasswordInput(Member member) {
+        String password = input("# 비밀번호 확인: ");
+        if (member.password.equals(password)) {
+            System.out.println("# 비밀번호가 일치합니다.");
+            stop();
+            return true;
+        }
+        return false;
+    }
+
+    private void showMember() {
+        Member member = checkEmailInput();
+        if (member != null) {
+            System.out.printf("# ID: %d번\n# 이메일: %s\n# 이름: %s\n# 성별: %s\n# 나이: %d세",
+                    member.memberId, member.email, member.memberName,
+                    member.gender, member.age);
+
+        }
     }
 
 
