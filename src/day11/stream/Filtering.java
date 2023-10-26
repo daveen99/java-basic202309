9b6f5c7a6da189f083d2dc14e39a0897e4d8ee71
다빈 <prospecs1039@daum.net>
2023-10-26 13:57:28 +0900

    # stream(스트림) 기초

---
+++ src/day11/stream/Filtering.java
@@ -0,0 +1,85 @@
+package day11.stream;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static day11.stream.Dish.Type.*;
+import static day11.stream.Menu.*;
+import static java.util.stream.Collectors.*;
+
+public class Filtering {
+
+    public static void main(String[] args) {
+
+        // stream의 filter
+        // 필터조건에 맞는 데이터들을 필터링하여 리스트로 반환
+
+        // 메뉴중에 채식주의자가 먹을 수 있는 요리를 필터링
+//        List<Dish> dishInVegtarian = menuList.stream()
+//                .filter(dish -> dish.isVegeterian())
+//                .collect(Collectors.toList());
+        //dishInVegtarian.forEach(dish -> System.out.println(dish));
+        menuList.stream()
+                .filter(dish -> dish.isVegeterian())
+                .collect(toList())
+                .forEach(dish -> System.out.println(dish));
+
+
+        System.out.println("===== 육류이면서 600칼로리 미만 =====");
+        menuList.stream()
+                .filter(dish -> dish.getType() == MEAT
+                && dish.getCalories() < 600)
+                .collect(toList())
+                .forEach(dish -> System.out.println(dish));
+
+        System.out.println("\n===== 요리중에 이름이 4글자인 요리만 =====");
+        menuList.stream()
+                .filter(d -> d.getName().length() == 4)
+                .collect(toList())
+                .forEach(d -> System.out.println(d));
+
+
+        System.out.println("\n===== 요리중에 300칼로리보다 큰것(초과) ====");
+        menuList.stream()
+                .filter(d -> d.getCalories() > 300)
+                .limit(3) // TOP 3를 추출
+                .collect(toList())
+                .forEach(d -> System.out.println(d));
+
+
+        System.out.println("====================");
+        menuList.stream()
+                .filter(d -> d.getCalories() > 300)
+                .skip(2)  // 맨 앞 2개를 제외시킴
+                .collect(toList())
+                .forEach(d -> System.out.println(d));
+
+
+
+        System.out.println("\n====== 메뉴목록에서 처음 등장하는 " +
+                            "2개의 생선요리 필터링 =====");
+        menuList.stream()
+                .filter(d -> d.getType() == FISH)
+                .limit(2)
+                .collect(toList())
+                .forEach(d -> System.out.println(d));
+
+        System.out.println();
+        List<Integer> numbers = List.of(1,2,1,3,3,2,4,4,5,6);
+        // 짝수만 필터링
+        List<Integer> filteredNumbers = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .distinct()
+                .collect(toList());
+        System.out.println(filteredNumbers);
+
+        // 중복제거
+//        Set<Integer> distinctedNumbers = new HashSet<>(filteredNumbers);
+//        System.out.println(distinctedNumbers);
+
+
+    }
+}
---
+++ src/day11/stream/Mapping.java
@@ -0,0 +1,90 @@
+package day11.stream;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static day11.stream.Dish.*;
+import static day11.stream.Menu.*;
+
+public class Mapping {
+
+    // 이름과 칼로리만 가진 클래스 설계
+    private static class SimpleDish {
+        private String name;
+        private int calories;
+
+        public SimpleDish(Dish dish) {
+            this.name = dish.getName();
+            this.calories = dish.getCalories();
+        }
+
+        public SimpleDish(String name, int calories) {
+            this.name = name;
+            this.calories = calories;
+        }
+
+        @Override
+        public String toString() {
+            return "SimpleDish{" +
+                    "name='" + name + '\'' +
+                    ", calories=" + calories +
+                    '}';
+        }
+    }
+
+    // 이름과 타입만 가진 클래스
+    private static class typeDish {
+        private String name;
+        private Type type;
+
+        public typeDish(Dish dish) {
+            this.name = dish.getName();
+            this.type = dish.getType();
+        }
+
+        public typeDish(String name, Type type) {
+            this.name = name;
+            this.type = type;
+        }
+
+        @Override
+        public String toString() {
+            return "typeDish{" +
+                    "name='" + name + '\'' +
+                    ", type=" + type +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // stream의 map: 컬렉션(list, set, map...)에서 원하는 정보만
+        //               추출하여 새로운 컬렉션으로 반환해줌
+        List<String> nameList = menuList.stream()
+                .map(dish -> dish.getName())
+                .collect(Collectors.toList());
+        System.out.println(nameList);
+
+        /*
+            요리목록에서 메뉴 이름과 칼로리를 추출하고 싶다.
+         */
+        menuList.stream()
+                .map(dish -> new SimpleDish(dish))
+                .collect(Collectors.toList())
+                .forEach(d -> System.out.println(d));
+
+        System.out.println();
+        System.out.println("=========");
+        /*
+            메뉴 목록에서 칼로리가 500칼로리보다 큰
+            음식들을 필터링한 다음에 음식의 이름과 타입만
+            추출해서 출력해주세요
+         */
+        menuList.stream()
+                .filter(dish -> dish.getCalories() > 500)
+                .map(dish -> new typeDish(dish))
+                .collect(Collectors.toList())
+                .forEach(d -> System.out.println(d));
+    }
+}
